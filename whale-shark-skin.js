0b63e30ad77b28d67fd052fcb9cb98d8c218225b
Richard Mavis <rmavis@gmail.com>
2018-07-15 14:22:21 -0700

    Revise and add documentation.

--- whale-shark-skin.js
+++ whale-shark-skin.js
@@ -1,32 +1,53 @@
+/*
+ * WhalesharkSkin :: args -> HTMLElement
+ * args = An object containing any of these key-value pairs:
+ *   cols = (int|[int,int]) the number of columns
+ *   line = (string|[[int,int]{3}]) the border color
+ *   boxColor = (string|[[int,int]{3}]) the box background color
+ *   dotColor = (string|[[int,int]{3}]) the dot color
+ *   dotSize = (int|[int,int]) the size of the dot as a percentage
+ *     of the box size
+ * For each of these parameters, a value can either be given directly
+ * (as a string or integer) or upper and lower bounds for randomization
+ * can be given in arrays. For colors, values will be set in HSL.
+ */
 function Whalesharkskin(args) {
 
+    // init :: args -> HTMLElement
     function init(args) {
         return makeSquares(getGridStyles(args));
     }
 
-    function getGridStyles(conf) {
-        var cols = makeArgMaker(conf.cols, makeNumberRandomizer, makeIdentity(20))();
-        var size = Math.ceil(window.screen.width / cols);
-        var rows = Math.ceil(window.screen.height / cols);
-        var line = makeArgMaker(conf.line, makeIdentity, makeIdentity('hsl(0, 0%, 50%)'));
-        var boxColor = makeArgMaker(conf.boxColor, makeColorRandomizer, makeIdentity('hsl(0, 0%, 100%)'));
-        var dotColor = makeArgMaker(conf.dotColor, makeColorRandomizer, makeIdentity('hsl(0, 0%, 0%)'));
-        var dotSize = makeArgMaker(conf.dotSize, makePercentRandomizer, makeIdentity(50));
+    // getGridStyles :: args -> conf
+    // conf = An object containing key-value pairs for building the
+    //   elements. It's similar to an `args` object but its values
+    //   are functions, except for `cols`, `rows`, `size`. For the
+    //   others, the functions all accept one argument and return
+    //   a value appropriate for its purpose.
+    function getGridStyles(args) {
+        var width = Math.max(window.screen.width, window.outerWidth, document.body.offsetWidth);
+        var height = Math.max(window.screen.height, window.outerHeight, document.body.offsetHeight);
+
+        var cols = makeArgMaker(args.cols, makeNumberRandomizer, makeIdentity(20))();
+        var size = Math.ceil(width / cols);
+        var rows = Math.ceil(height / size);
+
         return {
             cols: cols,
-            rows: rows,
             size: size,
-            line: line,
-            boxColor: boxColor,
-            dotColor: dotColor,
-            dotSize: dotSize,
+            rows: rows,
+            line: makeArgMaker(args.line, makeIdentity, makeIdentity('hsl(0, 0%, 50%)')),
+            boxColor: makeArgMaker(args.boxColor, makeColorRandomizer, makeIdentity('hsl(0, 0%, 100%)')),
+            dotColor: makeArgMaker(args.dotColor, makeColorRandomizer, makeIdentity('hsl(0, 0%, 0%)')),
+            dotSize: makeArgMaker(args.dotSize, makePercentRandomizer, makeIdentity(50)),
         };
     }
 
+    // makeSquares :: conf -> HTMLElement
     function makeSquares(conf) {
-        var wrap = makeWrap();
+        var wrap = makeWrap(conf);
         for (var o = 0; o < conf.rows; o++) {
-            var row = makeRow();
+            var row = makeRow(conf);
             for (var i = 0; i < conf.cols; i++) {
                 row.appendChild(makeSquare(conf));
             }
@@ -35,7 +56,8 @@ function Whalesharkskin(args) {
         return wrap;
     }
 
-    function makeWrap() {
+    // makeWrap :: conf -> HTMLElement
+    function makeWrap(conf) {
         return makeElement(
             'div',
             {
@@ -46,8 +68,8 @@ function Whalesharkskin(args) {
                     transform: 'translate(-50%,-50%)',
                     margin: '0',
                     padding: '0',
-                    minWidth: '100%',
-                    minHeight: '100%',
+                    width: (conf.size * conf.cols) + 'px',
+                    height: (conf.size * conf.rows) + 'px',
                     fontSize: '0',
                 },
             },
@@ -55,14 +77,16 @@ function Whalesharkskin(args) {
         );
     }
 
-    function makeRow() {
+    // makeRow :: conf -> HTMLElement
+    function makeRow(conf) {
         return makeElement(
             'div',
             {
                 style: {
                     display: 'block',
                     position: 'relative',
-                    minWidth: '100%',
+                    width: (conf.size * conf.cols) + 'px',
+                    height: conf.size + 'px',
                     fontSize: 0,
                     whiteSpace: 'nowrap',
                 },
@@ -71,8 +95,8 @@ function Whalesharkskin(args) {
         );
     }
 
+    // makeSquare :: conf -> HTMLElement
     function makeSquare(conf) {
-        var dot_size = conf.dotSize(conf.size);
         return makeElement(
             'div',
             {
@@ -86,26 +110,37 @@ function Whalesharkskin(args) {
                     overflow: 'visible',
                 },
             },
-            makeElement(
-                'div',
-                {
-                    style: {
-                        display: 'block',
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        height: 0,
-                        width: 0,
-                        border: (dot_size / 2) + 'px solid ' + conf.dotColor(),
-                        borderRadius: (dot_size / 2) + 'px',
-                        zIndex: 1,
-                    },
-                }
-            )
+            makeDot(conf)            
         );
     }
 
+    // makeDot :: conf -> HTMLElement
+    function makeDot(conf) {
+        var dot_size = conf.dotSize(conf.size);
+        return makeElement(
+            'div',
+            {
+                style: {
+                    display: 'block',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    height: 0,
+                    width: 0,
+                    border: (dot_size / 2) + 'px solid ' + conf.dotColor(),
+                    borderRadius: (dot_size / 2) + 'px',
+                    zIndex: 1,
+                },
+            },
+            null
+        );
+    }
+
+    // makeElement :: (string, attrs, HTMLElement) -> HTMLElement
+    // attrs = An object containing an object containing key-value
+    //   pairs to be set as the element's attributes. Mostly useful
+    //   for setting styles, but could be used for other things.
     function makeElement(tag, attrs, child) {
         var elem = document.createElement(tag);
         for (var attr in attrs) {
@@ -123,8 +158,9 @@ function Whalesharkskin(args) {
         return elem;
     }
 
+    // makeColorRandomizer :: [[int,int]{3}] -> b -> string
     function makeColorRandomizer(x) {
-        return function () {
+        return function (n) {
             return 'hsl('+
                 getRandomInt(x[0][0], x[0][1])+','+
                 getRandomInt(x[1][0], x[1][1])+'%,'+
@@ -132,24 +168,28 @@ function Whalesharkskin(args) {
         }
     }
 
+    // makeNumberRandomizer :: [int,int] -> b -> int
     function makeNumberRandomizer(x) {
-        return function () {
+        return function (n) {
             return getRandomInt(x[0], x[1]);
         }
     }
 
+    // makePercentRandomizer :: [int,int] -> int -> int
     function makePercentRandomizer(x) {
         return function (n) {
             return ((n / 100) * getRandomInt(x[0], x[1]));
         };
     }
 
+    // makeIdentity :: a -> b -> a
     function makeIdentity(x) {
-        return function () {
+        return function (n) {
             return x;
         };
     }
 
+    // makeArgmaker :: (a, (b -> c), (b -> c)) -> (c -> d) -> d
     function makeArgMaker(arg, maker, alt) {
         if (arg instanceof Array) {
             return maker(arg);
@@ -162,7 +202,6 @@ function Whalesharkskin(args) {
         }
     }
 
-
     // via https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     function getRandomInt(min, max) {
         min = Math.ceil(min);
@@ -170,7 +209,5 @@ function Whalesharkskin(args) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-
-    // Will need to return an element containing the image.
     return init((args || {}));
 }
